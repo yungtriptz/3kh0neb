0228a5a03603068ce0ee287ad85abaf104d902a6
yungtriptz <omaranmed658@gmail.com>
2026-07-01 01:01:17 +0400

    Update main.js

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,9 +1,6 @@
-// Poko - Main Script
 console.log("Welcome to Poko!");
 
-// Set site name in footer
 document.addEventListener('DOMContentLoaded', function() {
-    // Update footer
     var footer = document.querySelector('footer');
     if (footer) {
         footer.textContent = "© Poko 2024";
